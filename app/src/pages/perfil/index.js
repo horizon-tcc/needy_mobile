a58245d4fe202e11b6c5e0b4834da64256c7382f
Manocchio <guruu.leste@gmail.com>
2020-04-06 23:04:24 -0300

    tela de perfil atualizado, ainda não finalizada

--- app/src/pages/perfil/index.js
+++ app/src/pages/perfil/index.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import styles from './styles'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default class Profile extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
 				<View style={styles.profileBackground}>
-					<View style ={styles.profilePic} />
+					<Image
+						style={styles.profilePic}
+						source={{ uri: 'https://scontent.fgru6-1.fna.fbcdn.net/v/t1.0-9/83743190_2660059810782257_5370719641435897856_n.jpg?_nc_cat=102&_nc_sid=85a577&_nc_eui2=AeHQEWPOJ1bkpoy1UdRwoZxkyu-U210QaFbK75TbXRBoVkshoJ0ZiH9Ab0piHkXCgfnCr1IJ02TosPJ-1NqdmwRR&_nc_oc=AQkAhx1MFSgpMj4_Lfy3YRR352IWwvBSHOvdjNyBTSkZAQjGq9P7nrLkMnHucfwf-TxqfFf_dTnBSQ4vy1i72s6L&_nc_ht=scontent.fgru6-1.fna&oh=5ffb66aa8e0a543f88ff5cd3e6fb9eed&oe=5EB24F03' }}
+					/>
 
-				
-				
+					<View style={styles.editBtn}>
+						<TouchableOpacity>
+							<FontAwesome5 name='pen' color='white' size={18} />
+						</TouchableOpacity>
+
+					</View>
+
+
+
+					<View style={styles.contentDiv}>
+						<Text style={styles.welcomeText}>
+							Olá, Manocchio
+						</Text>
+
+						<View style={styles.line} />
+					</View>
+
+					<View style={styles.achivContainer}>
+						<Image 
+							source={{uri:''}}
+							style={styles.}
+						/>
+					</View>
+
+
 				</View>
 			</View>
 		);
--- app/src/pages/perfil/styles.js
+++ app/src/pages/perfil/styles.js
@@ -10,22 +10,70 @@ const styles = StyleSheet.create({
     },
 
     profileBackground :{
+      justifyContent: 'flex-start',
       backgroundColor:'#fff',
       width:  width,
-      height: height - 200,
+      height: height - 145,
       borderTopLeftRadius: 60
 
     },
 
     profilePic: {
-      backgroundColor: '#0f0',
+      backgroundColor: '#979797',
       position: "absolute",
-      marginLeft: 60,
+      marginLeft: 45,
       top: -50,
       height: 120,
       borderRadius:60,
       width: 120,
+    },
+
+    
+    editBtn: {
+      backgroundColor: '#ec6262',
+      position: "absolute",
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginLeft: width - 43,
+      height: 43,
+      borderBottomLeftRadius: 20,
+      width: 43,
+    },
+
+
+    contentDiv: {
+      alignItems:'center',
+      marginTop: 30,
+      marginLeft: 130,
+    },
+
+    welcomeText: {
+      fontSize: 23,
+      color: '#ca2929',
+      fontWeight: "bold",
+      
+    },
+
+    line: {
+      marginTop: 10,
+      height: 1,
+      width: 145,
+      backgroundColor:'#ca2929'
+    },
+
+    achivContainer: {
+      marginTop: 35 ,
+      alignSelf:'center',
+      alignItems:'center',
+      width: width - 100,
+      height: 210,
+      borderColor: '#b8b8b8',
+      borderWidth: 1,
+      borderRadius: 20,
     }
+
+
+
 
 
 })
