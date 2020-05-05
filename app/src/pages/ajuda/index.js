ebfe2d593cf15cc43f14dcc3600227a5f1c28872
miguelrisquelme <miguelnascimento22@hotmail.com>
2020-05-05 11:05:02 -0300

    Com erro

--- app/src/pages/ajuda/index.js
+++ app/src/pages/ajuda/index.js
@@ -1,38 +1,59 @@
 import React from "react";
 import { View, Text, FlatList } from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
+import { TapGestureHandler } from "react-native-gesture-handler";
 const data_ajuda = [
 	{
 		id: 1,
 		opcao: "FAQ",
+		tela: "Home",
+	},
+	{
+		id: 2,
+		opcao: "Fale Conosco",
+		tela: "Home",
+	},
+	{
+		id: 3,
+		opcao: "Termos e Política de Privacidade",
+		tela: "Home",
+	},
+	{
+		id: 4,
+		opcao: "Dados do Aplicativo",
+		tela: "Home",
 	},
 ];
 
-function Ajuda({ opcao }) {
+function Ajuda({ opcao, tela }) {
 	return (
-		<View
-			style={{
-				backgroundColor: "#fff",
-				padding: 20,
-				borderBottomWidth: 0.5,
-				borderBottomColor: "#8c8c8c",
-
-				display: "flex",
-				flexDirection: "row",
-				justifyContent: "space-between",
-			}}
+		<TapGestureHandler
+			onHandlerStateChange={() => this.props.navigation.navigate(tela)}
 		>
-			<Text
+			<View
 				style={{
-					color: "#ca2929",
-					fontWeight: "bold",
-					fontSize: 15,
+					backgroundColor: "#fff",
+					padding: 20,
+					borderBottomWidth: 0.5,
+					borderBottomColor: "#8c8c8c",
+
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "space-between",
 				}}
 			>
-				{opcao}
-			</Text>
-			<AntDesign name="arrowright" size={18} color="#8c8c8c" />
-		</View>
+				<Text
+					style={{
+						color: "#ca2929",
+						fontWeight: "bold",
+						fontSize: 15,
+					}}
+				>
+					{opcao}
+				</Text>
+				<AntDesign name="arrowright" size={18} color="#8c8c8c" />
+			</View>
+		</TapGestureHandler>
 	);
 }
 
@@ -41,19 +62,9 @@ function help() {
 		<View>
 			<FlatList
 				data={data_ajuda}
-				renderItem={({ item }) => <Ajuda opcao={item.opcao} />}
-			/>
-			<FlatList
-				data={data_ajuda}
-				renderItem={({ item }) => <Ajuda opcao={item.opcao} />}
-			/>
-			<FlatList
-				data={data_ajuda}
-				renderItem={({ item }) => <Ajuda opcao={item.opcao} />}
-			/>
-			<FlatList
-				data={data_ajuda}
-				renderItem={({ item }) => <Ajuda opcao={item.opcao} />}
+				renderItem={({ item }) => (
+					<Ajuda opcao={item.opcao} tela={item.tela} />
+				)}
 			/>
 		</View>
 	);
--- app/src/pages/maps/index.js
+++ app/src/pages/maps/index.js
@@ -1,241 +1,27 @@
 // Ícones feitos por <a href="https://www.flaticon.com/br/autores/kiranshastry" title="Kiranshastry">Kiranshastry</a> from <a href="https://www.flaticon.com/br/" title="Flaticon"> www.flaticon.com</a>
 // Ícones feitos por <a href="https://www.flaticon.com/br/autores/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/br/" title="Flaticon"> www.flaticon.com</a>
-import React from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import styles from './styles'
-import { View } from 'react-native';
+import React from "react";
+import MapView, { Marker } from "react-native-maps";
+import styles from "./styles";
+import { View } from "react-native";
+import Markers from "../../components/Markers";
 
 export default class Maps extends React.Component {
 	render() {
 		return (
-
 			<View style={styles.container}>
-			<MapView
-				style={styles.mapStyle}
-				initialRegion={{
-					latitude: -23.5530698,
-					longitude: -46.4002202,
-					latitudeDelta: 0.0043,
-					longitudeDelta: 0.0034
-				}}
-			>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5584294,
-						longitude: -46.6348969,
+				<MapView
+					style={styles.mapStyle}
+					initialRegion={{
+						latitude: -23.5530698,
+						longitude: -46.4002202,
+						latitudeDelta: 0.0043,
+						longitudeDelta: 0.0034,
 					}}
-					title={"Hemocentro São Lucas - Unidade Liberdade"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5575896,
-						longitude: -46.6689441,
-					}}
-					title={"Fundação Pró-Sangue Hemocentro de São Paulo"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5438673,
-						longitude: -46.650106,
-					}}
-					title={"Hemocentro da Santa Casa de São Paulo"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.4678736,
-						longitude: -46.5249643,
-					}}
-					title={"Hemocentro São Lucas - Guarulhos"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5519394,
-						longitude: -46.3095214,
-					}}
-					title={"Hemocentro Suzano"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.595087,
-						longitude: -46.6457452,
-					}}
-					title={"Hemocentro HSP Unifesp"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5805124,
-						longitude: -46.6796303,
-					}}
-					title={"Fundação Hemocentro de Ribeirão Preto"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.6055957,
-						longitude: -46.6780212,
-					}}
-					title={"Hemocentro São Lucas - Unidade Vila Olimpia"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-
-				<Marker
-					coordinate={{
-						latitude: -23.6988796,
-						longitude: -46.557605,
-					}}
-					title={"Colsan Hemocentro Regional de São Bernardo do Campo"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.6686605,
-						longitude: -46.5298258,
-					}}
-					title={"Hemocentro São Lucas - Unidade Santo André"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5545718,
-						longitude: -46.4659528,
-					}}
-					title={"Banco de Sangue do Hospital Santa Marcelina"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-
-				<Marker
-					coordinate={{
-						latitude: -23.5533402,
-						longitude: -46.7439652,
-					}}
-					title={"Banco de Sangue de São Paulo"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5955197,
-						longitude: -46.6514364,
-					}}
-					title={"Banco de Sangue de São Paulo"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5901346,
-						longitude: -46.6730441,
-					}}
-					title={"Banco de Sangue Paulista"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.6560008,
-						longitude: -46.7055666,
-					}}
-					title={"Banco Sangue Paulista"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5662194,
-						longitude: -46.6379501,
-					}}
-					title={"A.C.Camargo Cancer Center Castro Alves Assistencial"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5700886,
-						longitude: -46.6420417,
-					}}
-					title={"Hospital Beneficência Portuguesa de São Paulo - Banco de Sangue"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.6187199,
-						longitude: -46.664189,
-					}}
-					title={"Colsan Associação Beneficente de Coleta de Sangue"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5790025,
-						longitude: -46.6796319,
-					}}
-					title={"Banco De Sangue Do Hospital Do Servidor Publico Estadual"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5972479,
-						longitude: -46.6566069,
-					}}
-					title={"Banco de Sangue - Unidade Einstein Morumbi"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5685683,
-						longitude: -46.6454757,
-					}}
-					title={"Instituto HOC de Hemoterapia"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.549864,
-						longitude: -46.661429,
-					}}
-					title={"Banco de Sangue de São Paulo e Serviços de Hemoterapia"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-
-				<Marker
-					coordinate={{
-						latitude: -23.5495421,
-						longitude: -46.6597153,
-					}}
-					title={"Banco de Sangue de São Paulo e Serviços de Hemoterapia"}
-					image={require('../../assets/images/telas/telaMaps/sangue.png')}
-				/>
-			</MapView>
-
+				>
+					<Markers />
+				</MapView>
 			</View>
 		);
 	}
 }
-
-
-
-
-
