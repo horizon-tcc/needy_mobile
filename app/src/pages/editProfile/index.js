6e596876e014b6fd33d434130ed8dcc90f4a21e1
miguelrisquelme <miguelnascimento22@hotmail.com>
2020-07-12 22:23:01 -0300

    alterando a tela

--- app/src/pages/editProfile/index.js
+++ app/src/pages/editProfile/index.js
@@ -4,23 +4,9 @@ import {View, Text, ScrollView, Image} from 'react-native';
 import { TextInput } from 'react-native-paper';
 import CardInfo from '../../components/CardInfo';
 import AuthContext from '../../contexts/auth';
-import ImagePicker from 'react-native-image-picker';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-
-
-const handleChooseAPhoto = () => {
-    const options = {
-        mediaTypes: 'Images',
-        allowsEditing: false,
-        base64: true,
-        quality: 0.5,
-        maxWidth: 500,
-        maxHeight: 500,
-    };
-    ImagePicker.showImagePicker(options, (response) => {
-        console.log(response);
-    });
-};
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
 
 const editProfile = () => {
     const {user} = React.useContext(AuthContext);
@@ -47,7 +33,36 @@ const editProfile = () => {
             setFoto("41fe627c50fbcfd50986b70158b6b493.jpg");
         }
 
+        getPermissionAsync = async () => {
+            if (Constants.platform.ios) {
+              const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+                if (status !== 'granted') {
+                    alert('Sorry, we need camera roll permissions to make this work!');
+                }
+            }
+        };
+
     }, [])
+
+
+    _pickImage = async () => {
+        try {
+            let result = await ImagePicker.showImagePicker({
+                mediaTypes: ImagePicker.MediaTypeOptions.All,
+                allowsEditing: true,
+                aspect: [4, 3],
+                quality: 0.5,
+                maxWidth: 500,
+                maxHeight: 500,
+            });
+            if (!result.cancelled) {
+                setFoto(result.uri);
+            }
+            console.log(result);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return(
         <ScrollView 
@@ -63,9 +78,9 @@ const editProfile = () => {
                 }}
             >
                 <TouchableOpacity
-                    onPress={handleChooseAPhoto}
+                    onPress={() => _pickImage()}
                 >
-                    <Image 
+                    {/* <Image 
                         source={{
                             uri: `https://needy-api.herokuapp.com/imagens/${foto}`
                         }}
@@ -75,7 +90,8 @@ const editProfile = () => {
                             height: 256,
                         }}
                         resizeMode="cover"
-                    />
+                    /> */}
+                    {foto && <Image source={{ uri: `https://needy-api.herokuapp.com/imagens/${foto}` }} style={{ width: 256, height: 256, borderRadius: 500, }} />}
                 </TouchableOpacity>
             </View>
 
